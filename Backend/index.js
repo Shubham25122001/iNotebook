c1555fe71068a96b59414f65048dab2e28988292
shubham <shubham25122001@gmail.com>
2023-02-18 21:11:41 +0530

    Lecture 44

--- Backend/index.js
+++ Backend/index.js
@@ -4,11 +4,14 @@
  connectToMongo();
  const app = express()
  const port = 3000
- app.get('/', (req, res) =>{
-    res.send('Hello world shubham')
- })
 
+ //Available Routes
+ app.use('/api/auth', require('./routes/auth'))
+ app.use('/api/notes', require('./routes/notes'))
+ 
  app.listen(port, () =>{
-    console.log(`Example app listening at https://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
  })
+ 
+
  
