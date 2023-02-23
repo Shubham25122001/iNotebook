81b9bdeb87bec701f387c1f801940b1e46a30144
shubham <shubham25122001@gmail.com>
2023-02-23 20:56:05 +0530

    Lecture 61

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -13,6 +13,96 @@ const NoteState = (props)=>{
       "__v": 0
     },
     {
+      "_id": "63f58f422d9d23c6124acf67",
+      "user": "63f4977b21bc46b13f690f45",
+      "title": "My Title",
+      "description": "Please wake up early",
+      "tag": "personal",
+      "date": "2023-02-22T03:42:58.036Z",
+      "__v": 0
+    },
+    {
+      "_id": "63f58f422d9d23c6124acf67",
+      "user": "63f4977b21bc46b13f690f45",
+      "title": "My Title",
+      "description": "Please wake up early",
+      "tag": "personal",
+      "date": "2023-02-22T03:42:58.036Z",
+      "__v": 0
+    },
+    {
+      "_id": "63f58f422d9d23c6124acf67",
+      "user": "63f4977b21bc46b13f690f45",
+      "title": "My Title",
+      "description": "Please wake up early",
+      "tag": "personal",
+      "date": "2023-02-22T03:42:58.036Z",
+      "__v": 0
+    },
+    {
+      "_id": "63f58f422d9d23c6124acf67",
+      "user": "63f4977b21bc46b13f690f45",
+      "title": "My Title",
+      "description": "Please wake up early",
+      "tag": "personal",
+      "date": "2023-02-22T03:42:58.036Z",
+      "__v": 0
+    },
+    {
+      "_id": "63f58f422d9d23c6124acf67",
+      "user": "63f4977b21bc46b13f690f45",
+      "title": "My Title",
+      "description": "Please wake up early",
+      "tag": "personal",
+      "date": "2023-02-22T03:42:58.036Z",
+      "__v": 0
+    },
+    {
+      "_id": "63f58f422d9d23c6124acf67",
+      "user": "63f4977b21bc46b13f690f45",
+      "title": "My Title",
+      "description": "Please wake up early",
+      "tag": "personal",
+      "date": "2023-02-22T03:42:58.036Z",
+      "__v": 0
+    },
+    {
+      "_id": "63f58f422d9d23c6124acf67",
+      "user": "63f4977b21bc46b13f690f45",
+      "title": "My Title",
+      "description": "Please wake up early",
+      "tag": "personal",
+      "date": "2023-02-22T03:42:58.036Z",
+      "__v": 0
+    },
+    {
+      "_id": "63f58f422d9d23c6124acf67",
+      "user": "63f4977b21bc46b13f690f45",
+      "title": "My Title",
+      "description": "Please wake up early",
+      "tag": "personal",
+      "date": "2023-02-22T03:42:58.036Z",
+      "__v": 0
+    },
+    {
+      "_id": "63f58f422d9d23c6124acf67",
+      "user": "63f4977b21bc46b13f690f45",
+      "title": "My Title",
+      "description": "Please wake up early",
+      "tag": "personal",
+      "date": "2023-02-22T03:42:58.036Z",
+      "__v": 0
+    },
+    {
+      "_id": "63f58f422d9d23c6124acf67",
+      "user": "63f4977b21bc46b13f690f45",
+      "title": "My Title",
+      "description": "Please wake up early",
+      "tag": "personal",
+      "date": "2023-02-22T03:42:58.036Z",
+      "__v": 0
+    },
+    {
       "_id": "63f58f422d9d23c6124acf69",
       "user": "63f4977b21bc46b13f690f45",
       "title": "My Title",
